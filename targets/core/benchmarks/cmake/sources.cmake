b752475b23b89582ef8c0317ed262f72ba91013f
Evgeniy Laputin <serpensbs@gmail.com>
2026-06-30 15:49:45 +0300

    dev: Бенчмарк FileReader'а

---
+++ targets/core/benchmarks/cmake/sources.cmake
@@ -0,0 +1,7 @@
+if(NOT DEFINED SOURCES_DIR)
+    message(FATAL_ERROR "Project sources directory not defined")
+endif()
+
+set(SOURCES ${SOURCES_DIR}/benchmarks/filesystem/file_reader_benchmark.cc
+            ${SOURCES_DIR}/utility/temp_file.cc
+)
